3551272e3d3d4bb21c866779255119bb1a170016
Joao Costa <a27931@alunos.ipca.com>
2025-01-06 00:47:50 +0000

    last

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/casino-game/", // Substitua pelo nome do repositório no GitHub
-});
+  server: {
+    port:3000,
+  },
+})
